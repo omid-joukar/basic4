2c1fa3ce77c3bbf0d327fe72e6a3de0e87b314cc
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-02 11:56:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by KPS on 7/2/2020.
+ */
+public class Main {
+    public static void main(String[] args) {
+        int w = -5 + (8 * 6);
+        int x = (55+9) % 9;
+        int y = 20 + ((-3*5) / 8);
+        int z= 5 + (15 / (3 * 2)) - (8 % 3);
+        System.out.println(w+"\n"+x+"\n"+y+"\n"+z);
+    }
+}
